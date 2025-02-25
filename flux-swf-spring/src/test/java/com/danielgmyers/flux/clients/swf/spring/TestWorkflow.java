b47fc06ebd1479e093149af7e6b174aec1832e01
Dan Myers <damyers@amazon.com>
2025-02-24 16:24:00 -0800

    Bump the build system's java version to 17, but still target java 11 bytecode. This lets us build with the latest Spring, which targets java 17 bytecode. https://github.com/danielgmyers/flux-swf-client/issues/149
    Upgrade the Spring dependency for flux-swf-spring to 6.2.3.
    Add a unit test for the spring initialization helpers.
    Update the README for flux-swf-spring so it's accurate for flux 2.1.

---
+++ flux-swf-spring/src/test/java/com/danielgmyers/flux/clients/swf/spring/TestWorkflow.java
@@ -0,0 +1,34 @@
+/*
+ *   Copyright Flux Contributors
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License").
+ *   You may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+package com.danielgmyers.flux.clients.swf.spring;
+
+import com.danielgmyers.flux.step.WorkflowStep;
+import com.danielgmyers.flux.wf.Workflow;
+import com.danielgmyers.flux.wf.graph.WorkflowGraph;
+import com.danielgmyers.flux.wf.graph.WorkflowGraphBuilder;
+
+public class TestWorkflow implements Workflow {
+    @Override
+    public WorkflowGraph getGraph() {
+        WorkflowStep step = new TestWorkflowStep();
+
+        WorkflowGraphBuilder builder = new WorkflowGraphBuilder(step);
+        builder.alwaysClose(step);
+
+        return builder.build();
+    }
+}
